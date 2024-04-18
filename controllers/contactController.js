703071800cde13f5fb9a3fbd62ad617331df2bc8
farhathulla <farhathulla01@gmail.com>
2024-04-18 13:02:13 +0530

    mongodb connection

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,14 +1,16 @@
+const asyncHandler = require("express-async-handler")
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getAllContacts = (req, res) => {
+const getAllContacts = asyncHandler(async (req, res) => {
   res.status(201).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Create new contacts
 //@route GET /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   console.log("The request is", req.body);
   const { name, email, phone} = req.body;
   if(!name || !email || !phone){
@@ -16,28 +18,28 @@ const createContact = (req, res) => {
     throw new Error("All fields are mandotory")
   }
   res.status(201).json({ message: "Create contact" });
-};
+});
 
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(201).json({ message: `Get contact on ${req.params.id}` });
-};
+});
 
 //@desc Update contact
 //@route GET /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(201).json({ message: `Update contact on ${req.params.id}` });
-};
+});
 
 //@desc Delete contact
 //@route GET /api/contacts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(201).json({ message: `Delete contact on ${req.params.id}` });
-};
+});
 
 module.exports = {
   getAllContacts,
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
-const dotenv =  require("dotenv").config();
+const connectDb = require("./config/dbConnection");
+const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
-app.use('/api/contacts', require("./routes/contactRoute"));
+app.use("/api/contacts", require("./routes/contactRoute"));
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
